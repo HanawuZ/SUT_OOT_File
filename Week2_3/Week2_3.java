5198d773d4bb6ed0bfaecc246ac4eb6c121cf696
HanawuZ <b6302245@gmail.com>
2022-08-08 22:02:23 +0700

    first push

---
+++ Week2_3/Week2_3.java
@@ -0,0 +1,16 @@
+package Week2_3;
+import java.util.Scanner;
+
+public class Week2_3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);        
+        System.out.print("Enter a postive num: ");
+        int num = sc.nextInt();
+        if (num>=0) {
+            System.out.format("Square root of %d is %f\n",num,Math.sqrt(num));
+            System.out.format("Cube root of %d is %f\n",num,Math.cbrt(num));
+        }
+        else System.out.print("Negative number is not allowed");
+        sc.close();
+    }
+}
