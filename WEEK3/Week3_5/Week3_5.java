27c1dbecb23b5dea9c2ffa8834b860f9d5bf1118
HanawuZ <b6302245@gmail.com>
2022-08-10 19:06:57 +0700

    #

--- WEEK3/Week3_5/Week3_5.java
+++ WEEK3/Week3_5/Week3_5.java
@@ -1,14 +1,13 @@
 package Week3_5;        // Using Folder Week3_5 as this class's package.
 public class Week3_5 {
 
-    /* ########################### Main Function ########################### */
+    /* ################################# Main Function ################################# */
     public static void main(String[] args) {
 
         /* Initialize 2D array of int with 3 rows & 3 columns. */
         int[][] arr = {{10,20,30},
                        {40,50,60},
                        {70,80,90}};
-
         int row;            //* Int variable for row indexs.
         int column;         //* Int variable for column indexes.
         int sum = 0;        //* Int variable for result of summation.
@@ -18,8 +17,9 @@ public class Week3_5 {
         for (row = 0 ; row<arr.length ; row++){
 
             // Loop for passing columns of array at current row.
+            //* Calculate summation of numbers in each row.
             for (column = 0 ; column<arr[row].length; column++){
-                sum += arr[row][column];            //* Calculate summation of numbers in each row.
+                sum += arr[row][column];            
             }
 
             System.out.println("Summation of row "+row+" is "+sum);     // Print summation of current row.
